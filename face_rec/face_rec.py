52eaad920d661e5068323e918b890d9dba8fea8f
cmyui <cmyuiosu@gmail.com>
2021-10-13 17:23:10 -0400

    remove duplicate import

--- face_rec/face_rec.py
+++ face_rec/face_rec.py
@@ -1,7 +1,6 @@
 import os
 
 import cv2
-import face_recognition
 import face_recognition as fr
 import numpy as np
 
@@ -50,17 +49,17 @@ def classify_face(image_filename: str) -> list[str]:
     #img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
     #img = img[:,:,::-1]
 
-    face_locations = face_recognition.face_locations(img)
-    unknown_face_encodings = face_recognition.face_encodings(img, face_locations)
+    face_locations = fr.face_locations(img)
+    unknown_face_encodings = fr.face_encodings(img, face_locations)
 
     face_names = []
 
     for face_encoding in unknown_face_encodings:
         # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(faces_encoded, face_encoding)
+        matches = fr.compare_faces(faces_encoded, face_encoding)
 
         # use the known face with the smallest distance to the new face
-        face_distances = face_recognition.face_distance(faces_encoded, face_encoding)
+        face_distances = fr.face_distance(faces_encoded, face_encoding)
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
