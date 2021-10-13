4967af5495a3717978430ad493f59272485e6ffb
cmyui <cmyuiosu@gmail.com>
2021-10-13 17:22:54 -0400

    misc cleanup & consistency

--- face_rec/face_rec.py
+++ face_rec/face_rec.py
@@ -25,17 +25,16 @@ def get_encoded_faces() -> dict[str, np.ndarray]:
     return encoded
 
 
-def unknown_image_encoded(img: str) -> np.ndarray:
+def unknown_image_encoded(image_filename: str) -> np.ndarray:
     """
     encode a face given the file name
     """
-    face = fr.load_image_file("faces/" + img)
+    face = fr.load_image_file(f"faces/{image_filename}")
     encoding = fr.face_encodings(face)[0]
 
     return encoding
 
-
-def classify_face(im: str) -> list[str]:
+def classify_face(image_filename: str) -> list[str]:
     """
     will find all of the faces in a given image and label
     them if it knows what they are
@@ -47,7 +46,7 @@ def classify_face(im: str) -> list[str]:
     faces_encoded = list(faces.values())
     known_face_names = list(faces.keys())
 
-    img = cv2.imread(im, 1)
+    img = cv2.imread(image_filename, flags=cv2.IMREAD_COLOR)
     #img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
     #img = img[:,:,::-1]
 
@@ -55,16 +54,18 @@ def classify_face(im: str) -> list[str]:
     unknown_face_encodings = face_recognition.face_encodings(img, face_locations)
 
     face_names = []
+
     for face_encoding in unknown_face_encodings:
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(faces_encoded, face_encoding)
-        name = "Unknown"
 
         # use the known face with the smallest distance to the new face
         face_distances = face_recognition.face_distance(faces_encoded, face_encoding)
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
+        else:
+            name = "Unknown"
 
         face_names.append(name)
 
@@ -73,14 +74,12 @@ def classify_face(im: str) -> list[str]:
             cv2.rectangle(img, (left-20, top-20), (right+20, bottom+20), (255, 0, 0), 2)
 
             # Draw a label with a name below the face
-            cv2.rectangle(img, (left-20, bottom -15), (right+20, bottom+20), (255, 0, 0), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(img, name, (left -20, bottom + 15), font, 1.0, (255, 255, 255), 2)
+            cv2.rectangle(img, (left-20, bottom-15), (right+20, bottom+20), (255, 0, 0), cv2.FILLED)
+            cv2.putText(img, name, (left-20, bottom+15), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 2)
 
 
     # Display the resulting image
     while True:
-
         cv2.imshow('Video', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return face_names
